dcfa3638f852100718bd89e3639c0e4da3c24112
Coder-Towhid <islamsmtowhidul@gmail.com>
2024-02-09 12:07:07 +0600

    local storage added

--- Client/src/loginCall.js
+++ Client/src/loginCall.js
@@ -5,8 +5,10 @@ export const loginCall = async(userCredential, dispatch)=>{
     dispatch({type: "LOGIN_START"});
     try{
         const res = await axios.post(`${API_SERVER}/api/auth/login`, userCredential);
+        console.log("login call",  res.data)
         dispatch({type: "LOGIN_SUCCESS", payload: res.data});
-        console.log(res.data)
+
+ 
     }catch(err){
         dispatch({type: "LOGIN_FAILURE", payload: err});
     }
